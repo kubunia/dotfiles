57086ee45144ce19092a49a778c194a06f7133e4
arowinski <arowinski02@gmail.com>
2020-08-09 20:18:04 +0200

    FZF bind <C-Space> as abort
    Allow to close fzf with <C-Space> bind.

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -3,7 +3,7 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
-export FZF_DEFAULT_OPTS=''
+export FZF_DEFAULT_OPTS='--bind ctrl-space:abort'
 
 alias fzf="
   fzf \
