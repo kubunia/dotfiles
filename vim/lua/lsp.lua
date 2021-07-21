e415c3baf78d8f4d3fcbaefc166d9f343a12a10b
arowinski <arowinski02@gmail.com>
2021-07-21 21:24:42 +0200

    Vim - LSP add mapping on attach

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -19,11 +19,27 @@ local lsp = {
   vim = {}
 }
 
+local on_attach = function(_, _)
+  local function buf_map(lhs, rhs)
+    U.buf_nmap(lhs, '<cmd>lua  ' .. rhs .. '<CR>')
+  end
+
+  require'lsp_signature'.on_attach()
+
+  buf_map('gD', 'vim.lsp.buf.declaration()')
+  buf_map('gd', 'vim.lsp.buf.definition()')
+  buf_map('gr', 'vim.lsp.buf.references()')
+  buf_map('K', 'vim.lsp.buf.hover()')
+end
+
+local merge = U.dict.merge
 local function setup_servers()
   require'lspinstall'.setup()
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup(lsp[server] and lsp[server] or {})
+    local config = lsp[server] and lsp[server] or {};
+
+    require'lspconfig'[server].setup(merge({ on_attach = on_attach }, config))
   end
 end
 
@@ -31,6 +47,7 @@ setup_servers()
 
 require'lspinstall'.post_install_hook = function()
   setup_servers()
+
   vim.cmd("bufdo e")
 end
 
@@ -41,15 +58,17 @@ nvim_lsp.solargraph.setup {
   cmd = { "solargraph", "stdio" },
   settings = { solargraph = { useBundler = true } },
   filetypes = { "ruby" },
-  flags = { debounce_text_changes = 150 }
+  flags = { debounce_text_changes = 150 },
+  on_attach = on_attach
 }
 
 -- Typescript
 
 nvim_lsp.tsserver.setup {
-  root_dir = nvim_lsp.util.root_pattern(".git"),
-  on_attach = function(client)
+  root_dir = nvim_lsp.util.root_pattern(".git", "yarn.lock"),
+  on_attach = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
+    on_attach(client, bufnr)
   end,
   settings = { documentFormatting = false }
 }
--- vim/lua/utils.lua
+++ vim/lua/utils.lua
@@ -1,4 +1,4 @@
-U = { list = {} }
+U = { list = {}, dict = {} }
 
 local function map_keys(mode, command, value, opts)
   opts = opts or { noremap = true, silent = true }
@@ -9,8 +9,16 @@ end
 function U.nmap(command, value, opts) map_keys('n', command, value, opts) end
 function U.imap(command, value, opts) map_keys('i', command, value, opts) end
 
+function U.buf_nmap(command, value, opts) map_keys('n', command, value, opts) end
+
 function U.list.reduce(list, acc, fn)
   for k, v in ipairs(list) do acc = fn(acc, v, k) end
 
   return acc
+end
+
+function U.dict.merge(target, source)
+  for k, v in pairs(source) do target[k] = v end
+
+  return target;
 end
