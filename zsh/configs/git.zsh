ee2b7294b7e2cee42ef1f3b624a2d04a9697936f
arowinski <arowinski02@gmail.com>
2021-09-15 20:30:11 +0200

    ZSH - Clean up git aliases

--- zsh/configs/git.zsh
+++ zsh/configs/git.zsh
@@ -6,19 +6,17 @@ alias gcan="git commit --amend --no-edit"
 alias gph="git push"
 alias gphf="git push --force-with-lease"
 alias gpl="git pull"
-alias gplrb="git pull --rebase"
 
 alias gco="git checkout"
 
 alias gu="git reset"
 alias gres="git reset --soft HEAD^"
 
-alias gl="git log --pretty=format:\"%C(yellow)%h %C(magenta)%ad%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=short -20"
-alias gla="git log --pretty=format:\"%C(yellow)%h %C(magenta)%ad%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=short"
+alias gl="git log --pretty=format:\"%C(yellow)%h %C(magenta)%ad%Cred%d %Creset%s%Cblue [%an]\" --decorate --date=short -20"
+alias glf="git log --pretty=format:\"%C(yellow)%h %C(magenta)%ad%Cred%d %Creset%s%Cblue [%an]\" --decorate --date=short"
 alias glt='gl --graph --all'
 alias glg='git log -E -i --grep'
 alias gls='git log -S'
-alias glf='git log --oneline --'
 
 alias gs="git stash"
 alias gsu="git stash -u"
@@ -33,7 +31,6 @@ alias gd="git diff --color=always"
 alias gdc="git diff --cached --color=always"
 
 alias gcp='git cherry-pick'
-alias gcat='git cat-file'
 
 alias grb="git rebase"
 
@@ -41,7 +38,6 @@ alias gcon='git continue'
 alias gabort='git abort'
 alias gcl="git clean -fi"
 
-alias conf="git conflicted"
 alias gbcl="git-branch-clean"
 
 compdef g=git
