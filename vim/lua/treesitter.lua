e2d133e2c4daf92254f46c021dcfe1d486ef11fc
arowinski <arowinski02@gmail.com>
2021-12-01 21:19:36 +0100

    Vim - install additional treesitter languages

--- vim/lua/treesitter.lua
+++ vim/lua/treesitter.lua
@@ -1,5 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "ruby", "javascript", "html", "css", "lua", "bash" },
+  ensure_installed = {
+    "bash", "css", "go", "html", "http", "javascript", "json", "lua", "ruby",
+    "typescript", "vim", "yaml"
+  },
   indent = { enable = false },
   highlight = { enable = true, additional_vim_regex_highlighting = true },
   rainbow = { enable = true, extended_mode = true, max_file_lines = 1000 }
