0b44a0f8a23eeca8b340038dc2be16a8e8e8744d
arowinski <arowinski02@gmail.com>
2021-12-02 21:26:42 +0100

    Vim - drop lsp-colors plugin
    Used color scheme supports newest features.

--- vim/lua/diagnostics.lua
+++ vim/lua/diagnostics.lua
@@ -1,10 +1,3 @@
-require("lsp-colors").setup({
-  Error = "#db4b4b",
-  Warning = "#e0af68",
-  Information = "#0db9d7",
-  Hint = "#10B981"
-})
-
 require("trouble").setup({
   position = "bottom",
   height = 10,
