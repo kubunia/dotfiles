b069be089e889b832707bfa0f32315225f1f633f
arowinski <arowinski02@gmail.com>
2021-12-14 21:54:00 +0100

    Vim - replace EFM LSP with null-ls

--- vim/lua/efm/luafmt.lua
+++
@@ -1,4 +0,0 @@
-return {
-  formatCommand = "lua-format -i --indent-width=2 --spaces-inside-table-braces",
-  formatStdin = true
-}
--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -1,23 +1,21 @@
-local nvim_lsp = require('lspconfig')
-local efm_config = require('efm.config')
-local make_config = require('lsp.make_config')
-local on_attach = require('lsp.on_attach')
-local ts_utils_attach = require('lsp.ts_utils')
+local nvim_lsp = require("lspconfig")
+local make_config = require("lsp.make_config")
+local on_attach = require("lsp.on_attach")
+local ts_utils_attach = require("lsp.ts_utils")
 
-U.nmap('<leader>p', "<cmd>lua vim.lsp.buf.formatting()<CR>")
+U.nmap("<leader>p", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 
 local lsp_config = {
-  efm = efm_config,
   sumneko_lua = {
     settings = {
       Lua = {
-        diagnostics = { enable = true, globals = { "vim", "U", "MPairs" } }
-      }
-    }
+        diagnostics = { enable = true, globals = { "vim", "U", "MPairs" } },
+      },
+    },
   },
-  jsonls = { settings = { json = { schemas = require('lsp.json_schemas') } } },
+  jsonls = { settings = { json = { schemas = require("lsp.json_schemas") } } },
   yamlls = {},
-  html = { filetypes = { 'html' } },
+  html = { filetypes = { "html" } },
   vimls = {},
   svelte = {},
   solargraph = {
@@ -25,7 +23,7 @@ local lsp_config = {
     cmd = { "solargraph", "stdio" },
     settings = { solargraph = { useBundler = true } },
     filetypes = { "ruby" },
-    flags = { debounce_text_changes = 150 }
+    flags = { debounce_text_changes = 150 },
   },
   tailwindcss = {},
   tsserver = {
@@ -35,27 +33,50 @@ local lsp_config = {
       ts_utils_attach(client)
       on_attach(client, bufnr)
     end,
-    settings = { documentFormatting = false }
-  }
+    settings = { documentFormatting = false },
+  },
 }
 
 local lsp_installer = require("nvim-lsp-installer")
 
 lsp_installer.on_server_ready(function(server)
-  local config = lsp_config[server.name] and lsp_config[server.name];
+  local config = lsp_config[server.name] and lsp_config[server.name]
 
   server:setup(make_config(config))
 end)
 
-require'lspconfig'.solargraph.setup(make_config(lsp_config.solargraph))
+require("lspconfig").solargraph.setup(make_config(lsp_config.solargraph))
+
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.stylelint,
+    null_ls.builtins.diagnostics.stylelint,
+    null_ls.builtins.formatting.stylua.with({
+      extra_args = {
+        "--indent-type=Spaces",
+        "--indent-width=2",
+        "--column-width=80",
+      },
+    }),
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.diagnostics.yamllint,
+  },
+  on_attach = on_attach,
+})
 
 LspInstallDefined = function()
-  local exclude = { 'solargraph' }
+  local exclude = { "solargraph" }
   local required = vim.tbl_filter(function(element)
     return not vim.tbl_contains(exclude, element)
   end, vim.tbl_keys(lsp_config))
-  local installed = vim.tbl_map(function(v) return (v.name) end,
-                                lsp_installer.get_installed_servers())
+  local installed = vim.tbl_map(function(v)
+    return v.name
+  end, lsp_installer.get_installed_servers())
 
   for _, server in pairs(required) do
     if not vim.tbl_contains(installed, server) then
