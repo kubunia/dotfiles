6aaa52d7819037401ffb1320762fd6864188562b
arowinski <arowinski02@gmail.com>
2021-12-14 22:02:27 +0100

    Vim - fix scroll mapping

--- vim/lua/lsp/on_attach.lua
+++ vim/lua/lsp/on_attach.lua
@@ -1,21 +1,21 @@
 return function(client, _)
   local function buf_map(lhs, rhs)
-    U.buf_nmap(lhs, '<cmd>lua  ' .. rhs .. '<CR>')
+    U.buf_nmap(lhs, "<cmd>lua  " .. rhs .. "<CR>")
   end
 
-  require'lsp_signature'.on_attach({ use_lspsaga = true })
+  require("lsp_signature").on_attach({ use_lspsaga = true })
 
-  buf_map('gD', 'vim.lsp.buf.declaration()')
-  buf_map('gd', 'vim.lsp.buf.definition()')
-  buf_map('gt', 'vim.lsp.buf.references()')
+  buf_map("gD", "vim.lsp.buf.declaration()")
+  buf_map("gd", "vim.lsp.buf.definition()")
+  buf_map("gt", "vim.lsp.buf.references()")
 
-  buf_map('K', "require'lspsaga.hover'.render_hover_doc()")
-  buf_map('gh', "require'lspsaga.provider'.preview_definition()")
-  buf_map('gs', "require('lspsaga.signaturehelp').signature_help()")
-  buf_map('<Leader>d', "require'lspsaga.diagnostic'.show_line_diagnostics()")
-  buf_map('<C-u>', "require('lspsaga.action').smart_scroll_with_saga(1)")
-  buf_map('<C-d>', "require('lspsaga.action').smart_scroll_with_saga(-1)")
-  U.buf_nmap('<C-a>', "<cmd>Lspsaga code_action<cr>")
+  buf_map("K", "require'lspsaga.hover'.render_hover_doc()")
+  buf_map("gh", "require'lspsaga.provider'.preview_definition()")
+  buf_map("gs", "require('lspsaga.signaturehelp').signature_help()")
+  buf_map("<Leader>d", "require'lspsaga.diagnostic'.show_line_diagnostics()")
+  buf_map("<C-u>", "require('lspsaga.action').smart_scroll_with_saga(-1)")
+  buf_map("<C-d>", "require('lspsaga.action').smart_scroll_with_saga(1)")
+  U.buf_nmap("<C-a>", "<cmd>Lspsaga code_action<cr>")
 
   -- buf_map('K', "vim.lsp.buf.hover()")
   -- buf_map('gs', "vim.lsp.buf.signature_help()")
